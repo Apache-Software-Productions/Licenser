67bdf034a05682a8b2d455c1f562d6f75a67aed3
hussainaliya493 <33820240-hussainaliya493@users.noreply.replit.com>
2024-06-16 02:40:32 +0000

    Added everything

---
+++ Licencer.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# THIS SOFTWARE WAS INSPIRED BY AND BASED OFF OF THE WORK OF: Nishanth Shanmuhham. In this repository > https://github.com/nishanths/license
+# Authors/Developer of Licenser: Aeternusdio.
+# LICENSER IS DUAL LICENSED UNDER THE MIT & APACHE SOFTWARE PRODUCTIONS (ASP) LICENSE VERSION 2.
+#
+# Copyright (c) 2024 Apache Software Production. (ASP)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# SLOWFETCH IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+Version=1.0.0
+
+source ./Array.sh
+
+display_usage() {
+echo "Usage: ./Licenser.sh <Name of license> <File name>"
+}
+
+if [ $# -eq 1 ] && [ "$1" == "help" ]; then
+    echo "help"
+    exit 0
+fi
+
+if [ $# -lt 2 ]; then
+    display_usage
+    exit 1
+fi
+
+ARG1=$1
+ARG2=$2
+
+if [[ -v Templates[$ARG1] ]]; then
+    TEMPLATE_FILE="${Templates[$ARG1]}"
+
+    if [ -f "$TEMPLATE_FILE" ]; then
+        cat "$TEMPLATE_FILE" > "$ARG2"
+        echo "$ARG1 has been copied to $ARG2."
+    else
+        echo "Template file $TEMPLATE_FILE does not exist."
+        exit 1
+    fi
+else
+    echo "The specified license name ($ARG1) is not supported."
+    exit 1
+fi
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Licenser
-A command line license text generator inspired by and based off of [Nishanths repo](https://github.com/nishanths/license)
+![Maintained](https://img.shields.io/badge/Maintained-Yes-green) ![By me (Inspired btw)](https://img.shields.io/badge/By-AeternusDio-blue) ![ASP](https://img.shields.io/badge/ASP-blue)
+A simple, fast, cool, command line tool for making LICENSE files written in bash. NOTICE: This wasent orginaly my idea, Big credits go to [Nishanth Shanmuhham](https://github.com/nishanths) as he made [License (in go)](https://github.com/nishanths/license).
+
+## License
+Dual licensed under the Apache Software Production (ASP) License Version 2 & MIT license.
+
+## Devs/Collaborators
+AeternusDio - [Me](https://github.com/AeternusDio)
+
+## Usage
+First make sure you have your LICENSE file ready.
+then type into your command line:
+```./Licenser.sh <License> <File>```
+&lt;License&gt; - The License document you want to copy to the file.
+&lt;File&gt; - The file you want to copy &lt;License&gt; to.
+> If you aren’t sure of the names of the particular License documents then you can type: ./Licenser -help. which will pull up a man page including all the names of the License documents.
+
+--- Readme and Software by AeternusDio ---
+-
+
